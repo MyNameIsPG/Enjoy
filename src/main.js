d751385bb03a1f19c20496c07ee45e23521eb6fc
penggui <493871850@qq.com>
2018-04-18 17:39:05 +0800

    架构样式美化

--- src/main.js
+++ src/main.js
@@ -3,21 +3,19 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import $ from 'jquery'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
-
+import '@/css/public.styl'
 Vue.use(ElementUI);
-
-
 
 Vue.config.productionTip = false
 
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+    el: '#app',
+    router,
+    components: { App },
+    template: '<App/>'
 })
