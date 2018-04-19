9c25a0ca6cd7c0fee1d6932a91211033262c8616
penggui <493871850@qq.com>
2018-04-19 17:53:16 +0800

    上传功能的封装

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,86 +1,30 @@
 <template>
 	<div class="__login">
-		<nav class="navbar navbar-default">
-			<div class="container-fluid">
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-			        <span class="sr-only">Toggle navigation</span>
-			        <span class="icon-bar"></span>
-			        <span class="icon-bar"></span>
-			        <span class="icon-bar"></span>
-			      </button>
-					<a class="navbar-brand" href="#">Brand</a>
-				</div>
+		<upload :id=uploadId1></upload>
+		<br>
+		<upload :id=uploadId2 :multiple=multiple2></upload>
+		<br>
+		<upload :id=uploadId3></upload>
 
-				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-					<ul class="nav navbar-nav">
-						<li class="active">
-							<a href="#">Link <span class="sr-only">(current)</span></a>
-						</li>
-						<li>
-							<a href="#">Link</a>
-						</li>
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-							<ul class="dropdown-menu">
-								<li>
-									<a href="#">Action</a>
-								</li>
-								<li>
-									<a href="#">Another action</a>
-								</li>
-								<li>
-									<a href="#">Something else here</a>
-								</li>
-								<li role="separator" class="divider"></li>
-								<li>
-									<a href="#">Separated link</a>
-								</li>
-								<li role="separator" class="divider"></li>
-								<li>
-									<a href="#">One more separated link</a>
-								</li>
-							</ul>
-						</li>
-					</ul>
-					<form class="navbar-form navbar-left">
-						<div class="form-group">
-							<input type="text" class="form-control" placeholder="Search">
-						</div>
-						<button type="submit" class="btn btn-default">Submit</button>
-					</form>
-					<ul class="nav navbar-nav navbar-right">
-						<li>
-							<a href="#">Link</a>
-						</li>
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-							<ul class="dropdown-menu">
-								<li>
-									<a href="#">Action</a>
-								</li>
-								<li>
-									<a href="#">Another action</a>
-								</li>
-								<li>
-									<a href="#">Something else here</a>
-								</li>
-								<li role="separator" class="divider"></li>
-								<li>
-									<a href="#">Separated link</a>
-								</li>
-							</ul>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</nav>
+		<button>asdsa</button>
 	</div>
 </template>
 
 <script>
+import upload from '@/components/common/upload'
 export default {
-
+	components: {
+		upload
+	},
+	data(){
+		return {
+			uploadId1: 'test1',
+			uploadId2: 'test2',
+			multiple2: 'multiple',
+			uploadId3: 'test3',
+		}
+	},
+	
 }
 </script>
 
